abd25c9af0c83b5144e5eab9b6f354c77f4e081e
unknown <elimpejuniorekuh@gmail.com>
2022-02-16 05:23:50 +0100

    new side bar

--- app.js
+++ app.js
@@ -1,56 +1,39 @@
-const body = document.querySelector('body'),
-      sidebar = body.querySelector('nav'),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
 
+console.log(sidebarBtn);
 
-toggle.addEventListener("click" , () =>{
-    sidebar.classList.toggle("close");
-})
-
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
-    
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
-        modeText.innerText = "Dark mode";
-        
-    }
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
 });
 
 
+
 function call() {
-    var receiver = document.querySelector("input[name=tel]").value;
-    var amount = document.querySelector("input[name=number]").value;
-    localStorage.setItem("receiver", receiver);
-    localStorage.setItem("amount", amount);
+  var receiver = document.querySelector("input[name=tel]").value;
+  var amount = document.querySelector("input[name=number]").value;
+  localStorage.setItem("receiver", receiver);
+  localStorage.setItem("amount", amount);
 
 
-    if (!receiver || !amount) {
-        return;
-    }
-    localStorage.setItem("receiver", receiver);
-    localStorage.setItem("amount", amount);
+  if (!receiver || !amount) {
+      return;
+  }
+  localStorage.setItem("receiver", receiver);
+  localStorage.setItem("amount", amount);
 
-    var ussd = `*126*9*${receiver}*${amount}%23`;
+  var ussd = `*126*9*${receiver}*${amount}%23`;
 
-    window.open(`tel:${ussd}`);
+  window.open(`tel:${ussd}`);
 }
 
 
 
 function initialize() {
-    document.querySelector("#pay-btn").addEventListener("click", call);
-    var storedAmount = localStorage.getItem("amount");
-    document.getElementById("amount").value = storedAmount;
+  document.querySelector("#pay-btn").addEventListener("click", call);
+  var storedAmount = localStorage.getItem("amount");
+  document.getElementById("amount").value = storedAmount;
 
-    var storedReceiver = localStorage.getItem("receiver");
-    document.getElementById("receiver").value = storedReceiver;
+  var storedReceiver = localStorage.getItem("receiver");
+  document.getElementById("receiver").value = storedReceiver;
 }
